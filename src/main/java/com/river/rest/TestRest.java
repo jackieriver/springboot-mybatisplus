70cd75eb8337e10ed1da87e3a27a8e6c68fc74a2
hepengfei <river0416@126.com>
2017-12-23 01:09:39 +0800

    feat():测试jenkins的接口

---
+++ src/main/java/com/river/rest/TestRest.java
@@ -0,0 +1,20 @@
+package com.river.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/12/23.
+ */
+@RestController
+public class TestRest {
+    Logger log = LoggerFactory.getLogger(TestRest.class);
+
+    @RequestMapping("jenkins")
+    public String testJenkins(){
+        log.info("Hello Jenkins");
+        return "Hello Jenkins";
+    }
+}
